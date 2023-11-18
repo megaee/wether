832df74fd019fa7beaee78c3695156209b0ebfc5
Meganathan <47747348+megaee@users.noreply.github.com>
2023-11-18 08:47:49 +0530

    Update settings.py
    ww

--- My_Weather_Webapp/settings.py
+++ My_Weather_Webapp/settings.py
@@ -14,7 +14,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,4 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = Path(BASE_DIR / 'static')
+STATIC_ROOT = Path(BASE_DIR).joinpath('static')
+
